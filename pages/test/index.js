e036d07d601312d4839f496d83605bd8e1c5ce1f
João Vanzuita <joaovanzuita@me.com>
2019-07-13 21:42:08 -0300

    open graph update

--- pages/test/index.js
+++ pages/test/index.js
@@ -9,6 +9,15 @@ function Home() {
         <title>test 2</title>
         <meta property="og:title" content="testing 333" />
         <meta property="og:image" content={BlumenauBlog} />
+
+        <meta name="twitter:title" content="TITLE OF POST OR PAGE" />
+        <meta
+          name="twitter:description"
+          content="DESCRIPTION OF PAGE CONTENT"
+        />
+        <meta name="twitter:image" content={BlumenauBlog} />
+        <meta name="twitter:site" content="@USERNAME" />
+        <meta name="twitter:creator" content="@USERNAME" />
       </Head>
       <h1>Welcome to NextJs 9 !</h1>
     </div>
