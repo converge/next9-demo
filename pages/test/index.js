51430952c66998eaf58c756c5dcca03b86927714
João Vanzuita <joaovanzuita@me.com>
2019-07-13 21:57:45 -0300

    open graph update

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 import Head from 'next/head';
 import BlumenauBlog from '../../static/blumenau_blog.png';
+import BlumenauBlog2 from '../../static/blumenau_blog2.jpg';
 
 function Home() {
   return (
@@ -16,7 +17,7 @@ function Home() {
           name="twitter:description"
           content="DESCRIPTION OF PAGE CONTENT"
         />
-        <meta name="twitter:image" content={BlumenauBlog} />
+        <meta name="twitter:image" content={BlumenauBlog2} />
         <meta name="twitter:site" content="@netlify" />
         <meta name="twitter:site" content="netlify.com" />
         <meta name="twitter:creator" content="@joao_o" />
