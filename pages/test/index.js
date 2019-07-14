3b008754779f4a81c0ed024ff705bf6570e75f2d
João Vanzuita <joaovanzuita@me.com>
2019-07-13 21:49:21 -0300

    open graph update

--- pages/test/index.js
+++ pages/test/index.js
@@ -10,14 +10,16 @@ function Home() {
         <meta property="og:title" content="testing 333" />
         <meta property="og:image" content={BlumenauBlog} />
 
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="TITLE OF POST OR PAGE" />
         <meta
           name="twitter:description"
           content="DESCRIPTION OF PAGE CONTENT"
         />
         <meta name="twitter:image" content={BlumenauBlog} />
-        <meta name="twitter:site" content="@USERNAME" />
-        <meta name="twitter:creator" content="@USERNAME" />
+        <meta name="twitter:site" content="@netlify" />
+        <meta name="twitter:site" content="netlify.com" />
+        <meta name="twitter:creator" content="@joao_o" />
       </Head>
       <h1>Welcome to NextJs 9 !</h1>
     </div>
